0ecd9948488bd834cfebed1648248175805c1ac2
Pouya Mohammadi <pouya.mohammadi@postgrad.manchester.ac.uk>
2022-06-06 02:49:49 +0430

    add failing test

--- test_main.py
+++ test_main.py
@@ -6,12 +6,15 @@ class TestCase(unittest.TestCase):
 
     base = "http://127.0.0.1:5000/"
 
-    def test_hello_world(self):
-        response = requests.get(self.base + 'helloworld/pouya/1')
+#    def test_hello_world(self):
+#        response = requests.get(self.base + 'helloworld/pouya/1')
+#        print (response)
+#        self.assertEqual(200, response.status_code)
+
+    def testCLI(self):
+        response = requests.get(self.base + "cli/sh ip interface brief")
         print (response)
         self.assertEqual(200, response.status_code)
-
-
 
 if __name__ == '__main__':
     unittest.main()
