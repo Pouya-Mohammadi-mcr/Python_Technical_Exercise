68ede17457fec728248d555a421b0821d63c00f2
Pouya Mohammadi <pouya.mohammadi@postgrad.manchester.ac.uk>
2022-06-06 01:41:23 +0430

    passedTest

--- test_main.py
+++ test_main.py
@@ -7,7 +7,7 @@ class TestCase(unittest.TestCase):
     base = "http://127.0.0.1:5000/"
 
     def test_hello_world(self):
-        response = requests.get(self.base + 'helloworld' , {'name':'pouya', 'test':1})
+        response = requests.get(self.base + 'helloworld/pouya/1')
         print (response)
         self.assertEqual(200, response.status_code)
 
