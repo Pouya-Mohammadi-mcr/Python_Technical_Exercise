aa310c0f04d46d86bd845f9aa0b36b933534010f
Pouya Mohammadi <pouya.mohammadi@postgrad.manchester.ac.uk>
2022-06-06 04:21:17 +0430

    added more tests and exception handling

--- main.py
+++ main.py
@@ -14,11 +14,14 @@ class CLI(Resource):
         command = command
         ssh = paramiko.SSHClient()
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh.connect(host, port, username, password, look_for_keys=False)
-        stdin, stdout, stderr = ssh.exec_command(command)
-        lines = stdout.readlines()
-        formatted_lines = ' '.join(map(str, lines))
-        return formatted_lines
+        try:
+            ssh.connect(host, port, username, password, look_for_keys=False)
+            stdin, stdout, stderr = ssh.exec_command(command)
+            lines = stdout.readlines()
+            formatted_lines = ' '.join(map(str, lines))
+            return formatted_lines
+        except:
+            return 'Connection failed'
 
 api.add_resource(CLI, "/cli/<string:command>")
 
--- test_main.py
+++ test_main.py
@@ -6,10 +6,27 @@ class TestCase(unittest.TestCase):
 
     base = "http://127.0.0.1:5000/"
 
-    def testCLI(self):
-        response = requests.get(self.base + "cli/sh ip interface brief")
-        print (response.json())
+    def test_CLI_interface(self):
+        response = requests.get(self.base + "cli/sh ip int br")
+        self.assertTrue('Interface' and 'IP-Address' and 'Status' and 'Method' and 'Protocol' in response.text)
+
+    def test_CLI_other_commands(self):
+        response = requests.get(self.base + "cli/show debugging")
         self.assertEqual(200, response.status_code)
+
+    def test_CLI_inavlid_commands(self):
+        response = requests.get(self.base + "cli/show sth")
+        self.assertTrue('Line has invalid autocommand' in response.text)
+
+    def test_invalid_endpoint(self):
+        response = requests.get(self.base + "notfound/show debugging")
+        self.assertEqual(404, response.status_code)
+
+#Should be tested when credential are given by the user
+#    def test_CLI_connection_failed(self):
+#        response = requests.get(self.base + "cli/sh ip int br")
+#        print (response.text)
+#        self.assertEqual('"Connection failed"\n', response.text)
 
 if __name__ == '__main__':
     unittest.main()
